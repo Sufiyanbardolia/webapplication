7d4f35be0030ececa39ad88d301797a019b4390c
Sufiyanbardolia <124673609+Sufiyanbardolia@users.noreply.github.com>
2023-02-13 02:17:58 +0530

    new add todo list

--- src/ToDo3.jsx
+++ src/ToDo3.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { Button, TextField } from "@mui/material"
+import React, { useState } from "react"
 
-export const ToDo3 = () => {
-  return (
+export const ToDo3=()=>{
+  const [text,setText]=useState('')
+  const [todo,setToDo]=useState([])
+  const handleAddTask=()=>{
+    if(!text.trim())return
+    setToDo([...todo,text])
+    setText('') 
+  }
+  return(
     <div>
-        <h1>Helloooooooo</h1>
+      <h1>Helllooooo</h1>
+      <TextField placeholder="To DO List" type="text" value={text} onChange={(e)=>setText(e.target.value)}/>
+      <Button color="success" onClick={()=> handleAddTask()}>Add task</Button>
+      <ol>
+        {todo.map((item,index)=>{
+          return (
+          <li>{item}{''}</li>
+          )
+        })}
+      </ol>
     </div>
   )
 }
